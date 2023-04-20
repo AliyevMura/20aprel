e0c434d92428cb92a29b7f82305d2523fcf2341c
AliyevMura <7lvghl9@code.edu.az>
2023-04-20 10:05:30 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using _20aprilTasks.utilities.Exceptions;
+using System.Xml.Linq;
+
+namespace _20aprilTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int age = int.Parse(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new  AgeException("Sorry, Age must be greater than 0");
+                }
+            }
+            catch (AgeException ex)
+            {
+
+                Console.WriteLine(ex.Message); ;
+            }
+
+            
+        }
+    } 
+    
+}
